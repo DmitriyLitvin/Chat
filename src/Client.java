86b1dc59d8b74a9bad1b5cb73ccab10f5738aa9d
dmitriy <dimalitvin192.0@gmail.com>
2019-04-03 00:34:28 +0300

    added changes in Client ServerSomthing classes

--- src/Client.java
+++ src/Client.java
@@ -7,34 +7,74 @@ public abstract class Client implements Sendable {
     private BufferedReader in;
     private BufferedWriter out;
 
-    {
-        try {
-            clientSocket = new Socket("localhost", 8080);
-            reader = new BufferedReader(new InputStreamReader(System.in));
-            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
-        } catch (IOException e) {
+    private class Writer extends Thread {
 
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    String serverWord = in.readLine();
+                    if (serverWord.equals("stop")) {
+                        Client.this.downService();
+                        break;
+                    }
+                    System.out.println(serverWord);
+                } catch (IOException e) {
+                    downService();
+                }
+            }
+        }
+    }
+
+    private class Reader extends Thread {
+
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    String word = reader.readLine();
+
+                    if (word.equals("stop")) {
+                        out.write("stop" + "\n");
+                        Client.this.downService();
+                        break;
+                    } else {
+                        out.write(word + "\n");
+                    }
+                    out.flush();
+                } catch (IOException e) {
+                    downService();
+                }
+            }
         }
     }
 
     public void send() {
         try {
-            try {
-                System.out.println("Вы что-то хотели сказать? Введите это здесь:");
+            System.out.println("Вы что-то хотели сказать? Введите это здесь:");
 
-                while (true) {
-                    new Reader(reader, out).start();
-                    new Writer(in).start();
-                }
-            } finally {
-                System.out.println("Клиент был закрыт...");
+            clientSocket = new Socket("localhost", 8080);
+            reader = new BufferedReader(new InputStreamReader(System.in));
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+
+            new Reader().start();
+            new Writer().start();
+        } catch (IOException e) {
+            downService();
+        }
+    }
+
+
+    private void downService() {
+        try {
+            if (!clientSocket.isClosed()) {
                 clientSocket.close();
                 in.close();
                 out.close();
             }
-        } catch (IOException e) {
-            System.err.println(e);
+        } catch (IOException ignored) {
+
         }
     }
 }
--- src/ServerSomthing.java
+++ src/ServerSomthing.java
@@ -5,7 +5,6 @@ import java.util.LinkedList;
 import java.util.List;
 
 class ServerSomthing extends Thread {
-
     private Socket socket;
     private BufferedReader in;
     private BufferedWriter out;
@@ -20,15 +19,43 @@ class ServerSomthing extends Thread {
     @Override
     public void run() {
         String word;
+
         try {
             while (true) {
                 word = in.readLine();
+                if (word.equals("stop")) {
+                    this.downService();
+                    break;
+                }
                 System.out.println(word);
                 for (ServerSomthing vr : Server.serverList) {
-                    vr.send(word);
+                    if (vr == this) {
+                        continue;
+                    } else {
+                        vr.send(word);
+                    }
                 }
             }
         } catch (IOException e) {
+            e.getMessage();
+        }
+    }
+
+    private void downService() {
+        try {
+            if (!socket.isClosed()) {
+                socket.close();
+                in.close();
+                out.close();
+                for (ServerSomthing vr : Server.serverList) {
+                    if (vr.equals(this)) {
+                        vr.interrupt();
+                    }
+                    Server.serverList.remove(this);
+                }
+            }
+        } catch (IOException e) {
+            e.getMessage();
         }
     }
 
@@ -36,7 +63,8 @@ class ServerSomthing extends Thread {
         try {
             out.write(msg + "\n");
             out.flush();
-        } catch (IOException ignored) {
+        } catch (IOException e) {
+            e.getMessage();
         }
     }
 }
